f52397837356b80c86c8d15a44160e4c3d599cb2
sanyanema <snema2@illinois.edu>
2021-12-10 11:48:21 -0600

    Edited edge.h file

--- src/edge.h
+++ src/edge.h
@@ -2,3 +2,15 @@
 // setters first node and second node
 // setter for weight (distance from a to b), initially set it to -1
 // distance function here
+
+#include "node.h"
+
+class Edge {
+    public:
+        Edge();
+        Edge(Node first, Node second);
+        double calculateDistance();
+        double getEdgeWeight();
+    private:
+        double edgeWeight;
+};
