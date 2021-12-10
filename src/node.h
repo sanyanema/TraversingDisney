2271b73163c697e3563c04939f63720481d7fddd
sanyanema <snema2@illinois.edu>
2021-12-10 11:46:19 -0600

    Edited node.h file

--- src/node.h
+++ src/node.h
@@ -1,1 +1,16 @@
 // getter for wait time, latitude, long, name
+
+class Node {
+    public:
+        Node();
+        Node(int waitTime, double latitude, double longitude, std::string name);
+        int getWaitTime();
+        double getLatitude();
+        double getLongitude();
+        std::string getName();
+    private:
+        int waitTime;
+        double latitude;
+        double longitude;
+        std::string name;
+};
