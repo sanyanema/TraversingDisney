a7cd1c77e21fa2ae5108283ca34244b701f6de86
sanyanema <snema2@illinois.edu>
2021-12-13 16:23:21 -0600

    resolved conflict

--- src/BFS.cpp
+++ src/BFS.cpp
@@ -12,30 +12,11 @@ BFS::BFS() {
     //Input: Gph, G
     //Output: A labeling of the edges on G as discovery and cross edges
 }
-
-
-
-/**
-
-BFS::BFS(Graph & graph) {
-    graph_ = graph;
-
-    std::vector<bool> nodeExplore(fileRow);   //node_count_
-    //bfs checks row by row
-    std::vector<bool> t;
-    std::vector<std::vector<bool>> edgeExploredList;
-
-
-    sortEdges();
-    ds_ = DisjointSets();
-    ds_.makeDisjointSet(graph);
-}
-
-
-void BFS::bfsTraversal(std::string path) {
-    int fileRow = 0; //number of rows in the data
-    ifstream file(path);
-    String line;
+void BFS::bfsTraversal(std::string path, Graph * graph) {
+    int s = 0;     
+    int fileRow = 0;
+    std::ifstream file(path);
+    std::string line;
     while( getline(file, line) ) {
         fileRow++;
     }
@@ -61,19 +42,65 @@ void BFS::bfsTraversal(std::string path) {
         queue.pop_front();
 
     }
-
-    for(int i = 0;i < fileRow; i++) {
-        edgeExploredList.push_back(nodeRows);
-    }
-    //Nodes not explored yet
-    for (int i=0; i < fileRow; i++) {
-        if (!nodeExplore[i]) {
-            bfsTraversal(edgeExploredList, nodeExplore, i); 
-            //need a count for all connected components 
-            //connected_components++; ?????
+    for (i = adj[s].begin(); i !=adj[s].end(); ++i) {
+        if (!visited[*i]) {
+            visited[*i] = true;
+            queue.push_back(*i);
         }
     }
+
 }
+
+
+/**
+    std::vector<bool> nodeExplore(fileRow);   //node_count_
+    //bfs checks row by row
+    std::vector<bool> t;
+    std::vector<std::vector<bool>> edgeExploredList;
+    nodeRows(fileRow);
+
+    // for(int i = 0;i < fileRow; i++) {
+    //     //edgeExploredList.push_back(nodeRows);
+    // }
+    // //Nodes not explored yet
+    // for (int i=0; i < fileRow; i++) {
+    //     if (!nodeExplore[i]) {
+    //         bfsTraversal(edgeExploredList, nodeExplore, i);
+    //         //need a count for all connected components 
+    //         //connected_components++; ?????
+    //     }
+    // }
+}
+void BFS::bfsTraversal(std::vector<std::vector<bool>> &edgeExploredList, std::vector<bool> &nodeExplore, int Node) {
+    // std::queue<int> q;
+    // nodeExplore[Node] = true; //vertex
+    // bfsVertexList.push_back(Node);
+    // q.push(Node); //(vertex)
+
+    // //^above don't necessarily know what should be inside the () and []
+
+    // while (!q.empty()) {
+    //     int frontNode = q.front();
+    //     q.pop();
+    //     for(int col = 0; col < fileRow; col++) {
+    //         if (adjList[frontNode][col]) {  //adj_list_ dont know what to call in this
+    //             if(!nodeExplore[col]) { //geekforgeek if(!visited[*i])
+    //                 edgeExploredList[frontNode][col] = true;
+    //                 bfsEdgeList.pushback(std::pair<int, int>(frontNode, col));
+    //                 nodeExplore[col] = true;    //geekforgeek
+    //                 bfsVertexList.push_back(col);
+    //                 //queue.push_back(*i)
+    //                 q.push(col);
+    //             } else if (!edgeExploredList[frontNode][col]) {
+    //                 edgeExploredList[frontNode][col] = true;
+    //                 bfsEdgeList.pushback(std::pair<int, int>(frontNode, col));
+    //             }
+    //         }
+    //     }
+    // }
+}
+
+*/
 
 /**
 foreach (Vertex v : G.vertices()):
